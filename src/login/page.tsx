e52270ffa71f340b8dda8fdcbafc30f83cfecaa1
Paulo Nkeé João <154369410+paulo-pnj1@users.noreply.github.com>
2026-03-02 15:34:41 +0100

    Update page.tsx

--- src/login/page.tsx
+++ src/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { db } from '@/firebase';
+import { db } from '../firebase';
 import { ref, get, set } from 'firebase/database';
 import {
   Box,
